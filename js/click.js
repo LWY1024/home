280b1bbc4aa870a5006db6e7e2b75b43e1cf58e6
LWY1024 <guyunli8866@163.com>
2020-11-27 15:58:17 +0800

    '15.58'

--- js/click.js
+++ js/click.js
@@ -15,7 +15,7 @@ $('.region').on('touchstart',function(){
 
 
 //确定
-$('.kuai').on('touchstart',function(){
+$('.ensure').on('touchstart',function(){
     $('.region').siblings('ul').css('display','none');
     $('.region').css('color','#666');
     $('.region').next('img').attr('src','./image/SecondHandList/jt-down.png');
@@ -28,4 +28,54 @@ $('.mengcheng').on('touchstart',function(){
     $('.region').siblings('.li-box').css('display','none');
     $('.region').siblings('ul').css('display','none');
     $(this).hide();
+});
+
+
+$('.special').on('touchstart',function(){
+    $('.nolimit').find('.qy-box').hide();
+    $('.special a').addClass('organge metro');
+    $('.special').prev('.nolimit').find('a').removeClass('organge metro');
+    $(this).find('.qy-box').show();
+   
+});
+$('.nolimit').on('touchstart',function(){
+    $('.nolimit').next('.special').find('a').removeClass('organge metro');
+    $('.nolimit a').addClass('organge metro');
+});
+
+//价格
+$('.thousand').find('li a').on('touchstart',function(){
+    $('li a').removeClass('bcolor');
+    $(this).addClass('bcolor');
+});
+
+// 区域
+$('.area').find('span').on('touchstart',function(){
+    $('.area span').removeClass('bcolor');
+    $(this).addClass('bcolor');
+});
+// 面积
+$('.acreage').find('span').on('touchstart',function(){
+    $('.acreage span').removeClass('bcolor');
+    $(this).toggleClass('bcolor');
+});
+// 标签
+$('.tag').find('span').on('touchstart',function(){
+    $('.tag span').removeClass('bcolor');
+    $(this).toggleClass('bcolor');
+});
+// 楼龄
+$('.level').find('span').on('touchstart',function(){
+    $('.level span').removeClass('bcolor');
+    $(this).toggleClass('bcolor');
+});
+// 楼层
+$('.floor').find('span').on('touchstart',function(){
+    $('.floor span').removeClass('bcolor');
+    $(this).toggleClass('bcolor');
+});
+// 清空条件
+$('.resets').on('touchstart',function(){
+    $(this).removeClass('bcolor');
+    $('span').removeClass('bcolor');
 });
--- js/index.js
+++ js/index.js
@@ -5,3 +5,5 @@ function setRem(){
   }
   setRem();
   window.onresize = setRem;
+
+
